10539fe299a09ad6acc40f485a027a642237916a
JMG Studios <214383483+JMGstudios@users.noreply.github.com>
2026-08-26 00:06:12 +0200

    Added proper a proper credits field for the repo owners

--- core/projects/uxplay.sh
+++ core/projects/uxplay.sh
@@ -3,6 +3,7 @@
 PROJECT_ID="uxplay"
 PROJECT_NAME="UxPlay"
 PROJECT_DESCRIPTION="Open-source AirPlay mirror server for Linux, BSD, and macOS."
+PROJECT_SOURCE_URL="https://github.com/FDH2/UxPlay"
 PROJECT_INSTALL_SCRIPT="uxplay/install.sh"
 PROJECT_UNINSTALL_SCRIPT="uxplay/uninstall.sh"
 PROJECT_HOME_DIR="UxPlay"
